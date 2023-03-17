72d98e01f246512a0e77b3b87e2859140316a0c2
elon <elon4856@gmail.com>
2023-03-17 10:22:36 +0900

    feat: pong resp규격맞추기.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,36 +6,34 @@ import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args) {
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        System.out.println("Logs from your program will appear here!");
 
 //      Uncomment this block to pass the first stage
         ServerSocket serverSocket = null;
         Socket clientSocket = null;
         int port = 6379;
         try {
-          serverSocket = new ServerSocket(port);
-          serverSocket.setReuseAddress(true);
-          // Wait for connection from client.
-          clientSocket = serverSocket.accept();
+            serverSocket = new ServerSocket(port);
+            serverSocket.setReuseAddress(true);
+            // Wait for connection from client.
+            clientSocket = serverSocket.accept();
             Scanner scanner = new Scanner(clientSocket.getInputStream());
             String request = scanner.nextLine();
 
-            if (request.equals("PING")){
-                final OutputStream outputStream = clientSocket.getOutputStream();
-                outputStream.write("+PONG\r\n".getBytes(StandardCharsets.UTF_8));
-          }
+            final OutputStream outputStream = clientSocket.getOutputStream();
+            outputStream.write("+PONG\r\n".getBytes(StandardCharsets.UTF_8));
         } catch (IOException e) {
-          System.out.println("IOException: " + e.getMessage());
-        } finally {
-          try {
-            if (clientSocket != null) {
-              clientSocket.close();
-            }
-          } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
-          }
+        } finally {
+            try {
+                if (clientSocket != null) {
+                    clientSocket.close();
+                }
+            } catch (IOException e) {
+                System.out.println("IOException: " + e.getMessage());
+            }
         }
-  }
+    }
 }
