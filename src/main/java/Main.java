d19430f8ac5a7443185ef8ecca83aceeab061d64
elon <elon4856@gmail.com>
2023-03-31 11:00:54 +0900

    feat: pong resp규격맞추기.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,59 +1,56 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-
-    private static final int PORT = 6379;
-    private static final String STRING_START = "+";
-    private static final String RESPONSE_END = "\r\n";
-    private static final String PING_REQUEST = "ping";
-    private static final String PONG_RESPONSE = "PONG";
-
     public static void main(String[] args) {
         System.out.println("Logs from your program will appear here!");
         startServer();
     }
 
     private static void startServer() {
-        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(6379);
             serverSocket.setReuseAddress(true);
-            System.out.println("Server started on port " + PORT);
+            System.out.println("Server started on port 6379");
 
             while (true) {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Client connected: " + clientSocket.getInetAddress());
 
-                // Create a new thread to handle the client request
-                Thread clientThread = new Thread(() -> handleClientRequest(clientSocket));
+                Thread clientThread = new Thread(() -> {
+                    try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
+
+                        String line;
+                        while ((line = in.readLine()) != null) {
+                            if (line.equalsIgnoreCase("ping")) {
+                                out.println("+PONG\r\n");
+                            }
+                        }
+                        System.out.println("Client disconnected: " + clientSocket.getInetAddress());
+                    } catch (IOException e) {
+                        System.err.println("Error handling client request: " + e.getMessage());
+                    } finally {
+                        try {
+                            clientSocket.close();
+                        } catch (IOException e) {
+                            System.err.println("Error closing client socket: " + e.getMessage());
+                        }
+                    }
+                });
                 clientThread.start();
             }
         } catch (IOException e) {
             System.err.println("Error starting server: " + e.getMessage());
-        }
-    }
-
-    private static void handleClientRequest(Socket clientSocket) {
-        try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
-
-            String line;
-            while ((line = in.readLine()) != null) {
-                if (line.equalsIgnoreCase(PING_REQUEST)) {
-                    out.println(STRING_START + PONG_RESPONSE + RESPONSE_END);
-                }
-            }
-            System.out.println("Client disconnected: " + clientSocket.getInetAddress());
-        } catch (IOException e) {
-            System.err.println("Error handling client request: " + e.getMessage());
         } finally {
             try {
-                clientSocket.close();
+                if (serverSocket != null) {
+                    serverSocket.close();
+                }
             } catch (IOException e) {
-                System.err.println("Error closing client socket: " + e.getMessage());
+                System.err.println("Error closing server socket: " + e.getMessage());
             }
         }
     }
