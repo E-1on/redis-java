907dd9156b395d2ee997bbc5c23c1067a9141b65
elon <elon4856@gmail.com>
2023-04-07 09:12:33 +0900

    feat: 에코추가.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,81 +1,117 @@
+import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import memory.Memory;
 
 public class Main {
-
     public static void main(String[] args) {
         // You can use print statements as follows for debugging, they'll be visible
         // when running tests.
-        System.out.println("Logs from your program will appear here!");
-        final int port = 6379;
-
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
+        // System.out.println("Logs from your program will appear here!");
+        ServerSocket serverSocket = null;
+        int port = 6379;
+        try {
+            serverSocket = new ServerSocket(port);
             serverSocket.setReuseAddress(true);
-
+            // Wait for connection from client.
             while (true) {
-                Socket clientSocket;
-                try {
-                    clientSocket = serverSocket.accept();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-
-                new RedisConnection(clientSocket).start();
+                Socket clientSocket = serverSocket.accept();
+                Thread t = new Thread(new ClientHandler(clientSocket));
+                t.start();
             }
-        } catch (Exception e) {
-            System.out.println("error: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        } finally {
+            try {
+                if (serverSocket != null) {
+                    serverSocket.close();
+                }
+            } catch (IOException e) {
+                System.out.println("IOException: " + e.getMessage());
+            }
         }
     }
 
-    public static class RedisConnection extends Thread {
-
+    static class ClientHandler implements Runnable {
         private Socket clientSocket;
 
-        public RedisConnection(Socket clientSocket) {
+        public ClientHandler(Socket clientSocket) {
             this.clientSocket = clientSocket;
         }
 
-        @Override
+        private void handlePing(BufferedWriter writer) throws IOException {
+            writer.write("+PONG\r\n");
+            writer.flush();
+        }
+
+        private void handleEcho(BufferedWriter writer, String line)
+                throws IOException {
+            writer.write("+" + line + "\r\n");
+            writer.flush();
+        }
+
+    private void handleSet(BufferedWriter writer, String key, String value)
+        throws IOException {
+                  Memory.set(key, value);
+                  writer.write("+OK\r\n");
+                  writer.flush();
+                }
+
+            private void handleGet(BufferedWriter writer, String key)
+        throws IOException {
+                  String value = Memory.get(key);
+                  writer.write("+" + value + "\r\n");
+                  writer.flush();
+                }
+
         public void run() {
             try {
-                Scanner scanner = new Scanner(clientSocket.getInputStream());
-                OutputStream outputStream = clientSocket.getOutputStream();
-//                -        StringBuilder input = new StringBuilder();
-                    boolean isEchoCommand = false;
-
-                while (scanner.hasNext()) {
-                    String next = scanner.nextLine();
-                    System.out.println("next = " + next);
-//                    -          input.append(next);
-//
-//                    -          if (next.startsWith("ping")) {
-//                        -            String pong = "+PONG\r\n";
-//                        -            outputStream.write(pong.getBytes());
-//                        -          } else if (next.startsWith("DOCS")) {
-                                 if (next.equalsIgnoreCase("PING")) {
-                                        outputStream.write("+PONG\r\n".getBytes());
-                                      } else if (next.equalsIgnoreCase("DOCS")) {
-                            outputStream.write("+\r\n".getBytes());
-                                      } else if (next.equalsIgnoreCase("ECHO")) {
-                                        isEchoCommand = true;
-                                      } else if (isEchoCommand && next.matches("^(?![+\\-:$*]).+")) {
-                                        outputStream.write(("+" + next + "\r\n").getBytes());
-                        }
+                // BufferedReader reader = new BufferedReader(new
+                // InputStreamReader(clientSocket.getInputStream()));
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(clientSocket.getOutputStream()));
+                byte[] buffer = new byte[1024];
+                while (clientSocket.getInputStream().read(buffer) != -1) {
+                    String msg = new String(buffer);
+                    List<String> args =
+                            Arrays.stream(msg.split("\r\n"))
+                                    .filter(arg
+                                            -> !arg.trim().isEmpty() && !arg.startsWith("*") &&
+                                            !arg.startsWith("$"))
+                                    .collect(Collectors.toList());
+                    switch (args.get(0).toLowerCase()) {
+                        case "ping":
+                            handlePing(writer);
+                            break;
+                        case "echo":
+                            handleEcho(writer, args.get(1));
+                            break;
+                                  case "set":
+                                        handleSet(writer, args.get(1), args.get(2));
+                                        break;
+                                  case "get":
+                                        handleGet(writer, args.get(1));
+                                        break;
+                        default:
+                            break;
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println("IOException: " + e.getMessage());
+            } finally {
+                try {
+                    if (clientSocket != null) {
+                        clientSocket.close();
                     }
                 } catch (IOException e) {
                     System.out.println("IOException: " + e.getMessage());
-                } finally {
-                    try {
-                        if (clientSocket != null) {
-                            clientSocket.close();
-                        }
-                    } catch (IOException e) {
-                        System.out.println("IOException: " + e.getMessage());
-                    }
                 }
             }
         }
     }
+}
